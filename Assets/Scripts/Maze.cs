80a7a85fae4d16e92f0f1289c9af3c6b8478af81
Davy Peter Braun <hello@davybraun.com>
2017-07-10 18:46:52 +0200

    Larger walks thanks to implemented backtracking

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Maze : MonoBehaviour 
 {
@@ -19,12 +20,13 @@ public class Maze : MonoBehaviour
         WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
 
         cells = new MazeCell[size.x, size.z];
-        IntVector2 coordinates = RandomCoordinates;
+        List<MazeCell> activeCells = new List<MazeCell>();
+        
+        DoFirstGenerationStep(activeCells);
 
-        while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
+        while (activeCells.Count > 0) {
             yield return delay;
-            CreateCell(new IntVector2(coordinates.x, coordinates.z));
-            coordinates += MazeDirections.RandomValue.ToIntVector2();
+            DoNextGenerationStep(activeCells);
         }
     }
 
@@ -41,13 +43,34 @@ public class Maze : MonoBehaviour
         return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
     }
 
-    private void CreateCell (IntVector2 coordinates)
+    private void DoFirstGenerationStep (List<MazeCell> activeCells)
+    {
+        activeCells.Add(CreateCell(RandomCoordinates));
+    }
+
+    private void DoNextGenerationStep (List<MazeCell> activeCells)
+    {
+        int currentIndex = activeCells.Count - 1;
+        MazeCell currentCell = activeCells[currentIndex];
+        MazeDirection direction = MazeDirections.RandomValue;
+        IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2();
+        if (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
+            activeCells.Add(CreateCell(coordinates));
+        } else {
+            activeCells.RemoveAt(currentIndex);
+        }
+    }
+
+    private MazeCell CreateCell (IntVector2 coordinates)
     {
         MazeCell newCell = Instantiate<MazeCell>(cellPrefab);
         cells[coordinates.x, coordinates.z] = newCell;
+        newCell.coordinates = coordinates;
         newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
         newCell.transform.parent = transform;
         newCell.transform.localPosition = 
             new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+
+        return newCell;
     }
 }
